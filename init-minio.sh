efdf1ee5b43c7ec92d390653a2c60c62c63c052c
Martin80451 <martin.malmivirtahky@gmail.com>
2026-03-27 22:53:33 +0200

    Added MinIO init  and data reading scripts

---
+++ init-minio.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for MinIO to start..."
+
+until mc alias set minio http://minio:9000 user user12345 >/dev/null 2>&1 && mc admin info minio >/dev/null 2>&1; do
+  echo "MinIO not ready yet; waiting..."
+  sleep 2
+done
+
+echo "MinIO is ready! Configuring mc client..."
+
+echo "Creating bucket..."
+
+mc mb --ignore-existing minio/nyc-tlc-taxi-data
+
+echo "Uploading data..."
+
+mc cp /data/fhvhv_tripdata_2025-11.parquet minio/nyc-tlc-taxi-data
+
+echo "MinIO setup complete!"
+
+mc cp /data/fhvhv_tripdata_2025-11.parquet minio/nyc-tlc-taxi-data
+
+echo "MinIO setup complete!"
+
--- read_data.py
+++ read_data.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count, when
 
 spark = SparkSession.builder.appName("final_project").getOrCreate()
 
 df = spark.read.parquet("s3a://nyc-tlc-taxi-data/fhvhv_tripdata_2025-11.parquet", header=True)
-df.show()
+
+df_nulls_filled = df.fillna({"originating_base_num": "B03404"})
+
+df_nulls_filled.show(100)
