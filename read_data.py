39bd1cc61da413a1984d1c02232e7cff71a918c3
Martin80451 <martin.malmivirtahky@gmail.com>
2026-03-29 00:15:32 +0200

    Added top_routes and rides_per_day aggregations

--- read_data.py
+++ read_data.py
@@ -1,13 +1,25 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("final_project").getOrCreate()
-
 df = spark.read.parquet("s3a://nyc-tlc-taxi-data/raw/fhvhv_tripdata_2025-11.parquet", header=True)
+df_cleaned = df.fillna({"originating_base_num": "B03406"})
+df_cleaned.write.parquet("s3a://nyc-tlc-taxi-data/silver/fhvhv_tripdata_2025-11.parquet", mode="overwrite")
 
-df_nulls_filled = df.fillna({"originating_base_num": "B03406"})
+# Aggregations
 
-df_nulls_filled.write.parquet("s3a://nyc-tlc-taxi-data/silver/fhvhv_tripdata_2025-11.parquet", mode="overwrite")
+pickup_datetimes = df_cleaned.select("pickup_datetime")
+date_counts = []
+cols = ["pickup_date", "date_count"]
 
-# df_nulls_filled.count()
+for i in range(30):
+    i += 1
+    if len(str(i)) == 1: i = "0" + str(i)
+    date = "11-" + str(i)
+    date_count = df_cleaned.filter(df["pickup_datetime"].contains(date)).count()
+    date_counts.append((date, date_count))
 
-# df_nulls_filled.show(100)
+df2 = spark.createDataFrame(date_counts, schema=cols)
+df2.write.parquet("s3a://nyc-tlc-taxi-data/gold/rides_per_day.parquet", mode="overwrite")
+
+df3 = df_cleaned.select("PULocationID", "DOLocationID").groupBy(["PULocationID", "DOLocationID"]).count().orderBy("count", ascending=False)
+df3.write.parquet("s3a://nyc-tlc-taxi-data/gold/top_routes.parquet", mode="overwrite")
